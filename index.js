60f743321eab2471d7df22dd171717c9cad0c2b1
sillyangel <sillyangel3@gmail.com>
2023-01-31 12:36:55 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,79 @@
+(function (_0x5d4101, _0x817b88) {
+    const _0x3007b5 = _0xc876,
+        _0x30467a = _0x5d4101();
+    while (!![]) {
+        try {
+            const _0x3e9ae1 = parseInt(_0x3007b5(0xe2)) / 0x1 * (-parseInt(_0x3007b5(0xeb)) / 0x2) + -parseInt(_0x3007b5(0xe1)) / 0x3 + -parseInt(_0x3007b5(0xe6)) / 0x4 * (-parseInt(_0x3007b5(0xe4)) / 0x5) + -parseInt(_0x3007b5(0xdd)) / 0x6 * (parseInt(_0x3007b5(0xf0)) / 0x7) + parseInt(_0x3007b5(0xde)) / 0x8 + -parseInt(_0x3007b5(0xe0)) / 0x9 + parseInt(_0x3007b5(0xdc)) / 0xa;
+            if (_0x3e9ae1 === _0x817b88) break;
+            else _0x30467a['push'](_0x30467a['shift']());
+        } catch (_0x3d960f) {
+            _0x30467a['push'](_0x30467a['shift']());
+        }
+    }
+}(_0x5bf0, 0x450bf));
+
+function unblockedGetParentUrl() {
+    const _0x2e899a = _0xc876;
+    var _0x47df24 = parent !== window,
+        _0x1ffa7f = null;
+    _0x47df24 && (_0x1ffa7f = document[_0x2e899a(0xf7)]);
+    if (_0x1ffa7f != null && !_0x1ffa7f[_0x2e899a(0xed)](_0x2e899a(0xf6))) return null;
+    return _0x1ffa7f;
+}
+
+function _0x5bf0() {
+    const _0x1a2d30 = ['referrer', 'gameFrame', '<ul><li><a href=\'https://unblocked-games.s3.amazonaws.com/index.html\' target=\'_blank\'>Unblocked</a></l><li><a href=\'https://www.julgames.com/\' target=\'_blank\'>JulGames.com</a></l></ul>', '9222810hhDhvi', '5898KHbcfQ', '1464624cVDrRs', 'src', '4365747VoyoFC', '360693FShzuI', '20253TSsKgQ', 'getElementById', '5eusVGi', 'innerHTML', '775596LeMiKn', 'lichgames.s3.amazonaws.com', 'hostname', 'unblocked-games.s3.amazonaws.com', 'lichgames.weebly.com', '24axFjCN', 'https://www.freezenova.com', 'includes', 'girl-games.s3.amazonaws.com', 'Link', '1197Ryrhon', 'log', 'showLinks', 'JulGames', 'hidden', 'location', 'http'];
+    _0x5bf0 = function () {
+        return _0x1a2d30;
+    };
+    return _0x5bf0();
+}
+
+function unblockedRedirect(_0xd3e9b0) {
+    const _0x423869 = _0xc876,
+        _0x262d44 = unblockedGetParentUrl(),
+        _0x80817c = _0x423869(0xe9),
+        _0x36d8ab = _0x423869(0xea),
+        _0x501c4c = _0x423869(0xe7),
+        _0x347b46 = 'unblockedgames76x.weebly.com',
+        _0x1d6842 = _0x423869(0xee);
+    if (_0x262d44 != null) {
+        var _0x4e071c = new URL(_0x262d44)[_0x423869(0xe8)];
+        if (_0x4e071c !== _0x80817c && _0x4e071c !== _0x36d8ab && _0x4e071c !== _0x501c4c && _0x4e071c !== _0x347b46 && _0x4e071c !== _0x1d6842) return _0xd3e9b0 === undefined && (window[_0x423869(0xf5)] = _0x423869(0xec)), window[_0x423869(0xf2)] = !![], !![];
+        else window['showLinks'] = ![];
+    } else return window['showLinks'] = !![], !![];
+    return ![];
+}
+
+function unblockedOnLoadX(_0x18a15d) {
+    const _0x3398a8 = _0xc876;
+    if (_0x18a15d !== undefined) {
+        let _0x1c119c = _0x18a15d['getElementById']('links2');
+        _0x1c119c !== undefined && (window[_0x3398a8(0xf2)] && (_0x1c119c[_0x3398a8(0xf4)] = ![], _0x1c119c[_0x3398a8(0xe5)] += _0x3398a8(0xf9)));
+    }
+}
+
+function processJSONPResponse(_0x589c30) {}
+
+function _0xc876(_0x1b1b47, _0x56d92f) {
+    const _0x5bf033 = _0x5bf0();
+    return _0xc876 = function (_0xc876cf, _0x56b205) {
+        _0xc876cf = _0xc876cf - 0xdc;
+        let _0x2ccee5 = _0x5bf033[_0xc876cf];
+        return _0x2ccee5;
+    }, _0xc876(_0x1b1b47, _0x56d92f);
+}
+
+function unblockedOnLoadY(_0x2626ba, _0x2fa572, _0x118c25) {
+    const _0x2694d6 = _0xc876;
+    if (_0x2626ba !== undefined) {
+        let _0x5a1607 = _0x2626ba[_0x2694d6(0xe3)](_0x2694d6(0xf8));
+        if (_0x5a1607 !== undefined) {
+            const _0x17b368 = unblockedGetParentUrl();
+            if (_0x17b368 == null) {
+                console[_0x2694d6(0xf1)](_0x2694d6(0xef)), _0x5a1607['src'] = _0x118c25;
+                return;
+            }!unblockedRedirect(!![]) ? (console[_0x2694d6(0xf1)]('FN'), _0x5a1607[_0x2694d6(0xdf)] = _0x118c25) : (console[_0x2694d6(0xf1)](_0x2694d6(0xf3)), _0x5a1607['src'] = _0x2fa572);
+        }
+    }
+}
